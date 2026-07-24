f7a0b5d99c1355494edfb8b221578a9ca7dd873d
igorbdm <125665769+igorbdm@users.noreply.github.com>
2026-07-24 16:49:34 -0300

    Novos Canais Adicionados
    Audiotree, La Blogotheque (apenas os Take Away Shows) e Cultura Livre

--- src/channels.py
+++ src/channels.py
@@ -9,4 +9,19 @@ CHANNELS = {
         "keep": ["Tiny Desk Concert"],
         "ignore": [],
     },
+        "Audiotree": {
+        "id": "UCWjmAUHmajb1-eo5WKk_22A",
+        "keep": ["Full Session"],
+        "ignore": [],
+    },
+        "La Blogothèque (Take Away Shows)": {
+        "id": "UCFAKGci5lneha2x4XMbzYrQ",
+        "keep": ["A Take Away Show"],
+        "ignore": [],
+    },
+        "Cultura Livre": {
+        "id": "UCBuFf6FSDclfDvRGJY1Q5Uw",
+        "keep": ["CULTURA LIVRE"],
+        "ignore": [],
+    },
 }
